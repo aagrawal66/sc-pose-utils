996832d5430ad5c71912745ebc32ccfe91748ff2
Pete Lealiiee <petelealiieejr4@utexas.edu>
2026-03-06 16:27:35 -0600

    fixed bug (?), corrects assumption on coordinate representation of determined translation offset (?)

--- sc_pose/examples/check_repojection.py
+++ sc_pose/examples/check_repojection.py
@@ -20,7 +20,7 @@ offset_meta_data= data_file / "metadata" / "offset_results.json"
 camera_meta_data= data_file / "metadata" / "calibration.yaml" # wrong
 vicon_meta_data = data_file / "metadata" / "vicon_data.csv" # wrong
 skip_csv_header = True
-kps_file        = HERE / "artifacts" / "soho_reframed_mesh_pose_pack" / "mesh_points_1000.json"
+kps_file        = HERE / "artifacts" / "soho_reframed_mesh_pose_pack" / "mesh_points_50000.json"
 # setup keys
 res_path        = HERE / 'reproj_results'
 ##################################### Inputs #####################################
@@ -46,8 +46,9 @@ with open(offset_meta_data, 'r') as f:
 
 R_CCv = T_CvC[:3,:3]
 R_TTv = T_TvT[:3,:3]
-t_TTv = T_TvT[:3,3]
+
 t_CCv = T_CvC[:3,3]
+t_TTv = T_TvT[:3,3]
 
 
 # camera metadata
@@ -112,39 +113,88 @@ with open(vicon_meta_data,'r',newline='') as csv_f:
 
         Trfm_TvV        = q2trfm( soho_quatTvV )
         Trfm_CvV        = q2trfm( cam_quatCvV )
+        Trfm_VTv        = Trfm_TvV.T
+        Trfm_VCv        = Trfm_CvV.T
         Trfm_CCv        = R_CCv.T
         Trfm_TTv        = R_TTv.T
         Trfm_VC         = ( Trfm_CCv @ Trfm_CvV ).T
         Trfm_VT         = ( Trfm_TTv @ Trfm_TvV ).T
 
-        translation     = Trfm_VC.T \
+        
+        
+        # post-offset
+        translation_posto     = Trfm_VC.T \
                             @ ( \
-                                ( soho_TvV + Trfm_VT @ t_TTv ) \
-                              - ( cam_CvV  + Trfm_VC @ t_CCv ) \
+                                ( soho_TvV + Trfm_VTv @ t_TTv ) \
+                              - ( cam_CvV  + Trfm_VCv @ t_CCv ) \
                             )
-        Trfm_TC         =  ( Trfm_VT.T @ Trfm_VC )
-        quaternion      = rotm2q( Trfm_TC.T )
+        Trfm_TC_posto         =  ( Trfm_VT.T @ Trfm_VC )
+        quaternion_posto      = rotm2q( Trfm_TC_posto.T )
 
-        r_Co2To_CAM     = translation
-        q_TARGET_2_CAM  = quaternion
+        r_Co2To_CAM     = translation_posto
+        q_TARGET_2_CAM  = quaternion_posto
 
-        uv_cam          = proj.classless_pinhole_project_to_image(
-                                            q_TARGET_2_CAM = q_TARGET_2_CAM,
-                                            r_Co2To_CAM = r_Co2To_CAM,
+        # pre-offset
+        translation_preo     = Trfm_CvV \
+                            @ ( \
+                                soho_TvV  \
+                              - cam_CvV   \
+                            )
+        Trfm_TC_preo         =  ( Trfm_TvV @ Trfm_CvV.T )
+        quaternion_preo      = rotm2q( Trfm_TC_preo.T )
+
+
+
+
+        uv_cam_posto          = proj.classless_pinhole_project_to_image(
+                                            q_TARGET_2_CAM = quaternion_posto,
+                                            r_Co2To_CAM = translation_posto,
+                                            Kmat = Kmat,
+                                            BC_dist_coeffs = dist_coeffs,
+                                            points_xyz_TARGET = target_BFF_pts
+                                        )
+        
+        uv_cam_preo          = proj.classless_pinhole_project_to_image(
+                                            q_TARGET_2_CAM = quaternion_preo,
+                                            r_Co2To_CAM = translation_preo,
                                             Kmat = Kmat,
                                             BC_dist_coeffs = dist_coeffs,
                                             points_xyz_TARGET = target_BFF_pts 
                                         )
         
-        # fill this in with image info
+        uv_cam_origin          = proj.classless_pinhole_project_to_image(
+                                            q_TARGET_2_CAM = quaternion_posto,
+                                            r_Co2To_CAM = translation_posto,
+                                            Kmat = Kmat,
+                                            BC_dist_coeffs = dist_coeffs,
+                                            points_xyz_TARGET = np.zeros( (3,1) )
+                                        )
+        
         img             = draw_uv_points_on_image(
                                         img_or_path = img_path,
-                                        points_uv = uv_cam,
+                                        points_uv = uv_cam_posto,
+                                        point_color = (0, 255, 0),
+                                        point_radius = 15, 
+                                        point_thickness = -1
+                                    )
+        
+        img             = draw_uv_points_on_image(
+                                        img_or_path = img,
+                                        points_uv = uv_cam_preo,
                                         point_color = (0, 0, 255),
                                         point_radius = 15, 
                                         point_thickness = 2
-                                    
                                     )
+        
+        img             = draw_uv_points_on_image(
+                                        img_or_path = img,
+                                        points_uv = uv_cam_origin,
+                                        point_color = (255, 0, 0),
+                                        point_radius = 30, 
+                                        point_thickness = -1
+                                    )
+
+
         cv2.imwrite( res_path / f"pandas_check_{img_base}.png", img)
     
 
